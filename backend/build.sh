12d9f14f93b9576fe54ec165262359583e4673fb
Rohit-Gupta-IIC <rohit.gupta@iic.ac.in>
2025-09-30 12:04:40 +0530

    changes

--- backend/build.sh
+++ backend/build.sh
@@ -3,7 +3,10 @@ set -o errexit
 set -o nounset
 set -o pipefail
 
+SCRIPT_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
+cd "${SCRIPT_DIR}"
+
 python -m pip install --upgrade pip
-pip install -r requirements.txt
+python -m pip install -r requirements.txt
 
 python manage.py collectstatic --noinput
